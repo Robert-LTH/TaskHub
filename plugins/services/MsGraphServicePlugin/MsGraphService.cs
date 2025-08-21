90ac6d7729facc7a4d6348d6104d556ab92aa11c
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2025-08-21 08:45:56 +0200

    Refactor Graph service to use HttpClientFactory and secure Hangfire

--- plugins/services/MsGraphServicePlugin/MsGraphService.cs
+++ plugins/services/MsGraphServicePlugin/MsGraphService.cs
@@ -7,11 +7,18 @@ namespace MsGraphServicePlugin;
 
 public class MsGraphServicePlugin : IServicePlugin
 {
+    private readonly IHttpClientFactory _factory;
+
+    public MsGraphServicePlugin(IHttpClientFactory factory)
+    {
+        _factory = factory;
+    }
+
     public string Name => "msgraph";
 
     public async Task<string> GetAsync(string resource, CancellationToken cancellationToken)
     {
-        using var client = new HttpClient();
+        var client = _factory.CreateClient("msgraph");
         var endpoint = $"https://graph.microsoft.com/v1.0/{resource}";
         return await client.GetStringAsync(endpoint, cancellationToken);
     }
--- src/TaskHub.Server/PluginManager.cs
+++ src/TaskHub.Server/PluginManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
 using TaskHub.Abstractions;
 
 namespace TaskHub.Server;
@@ -10,7 +11,13 @@ public class PluginManager
 {
     private readonly Dictionary<string, (ICommandHandler Handler, PluginLoadContext Context, string AssemblyPath)> _handlers = new();
     private readonly List<string> _assemblies = new();
-    private IServicePlugin? _service;
+    private readonly IServiceProvider _provider;
+    private (IServicePlugin Plugin, PluginLoadContext Context, string AssemblyPath)? _service;
+
+    public PluginManager(IServiceProvider provider)
+    {
+        _provider = provider;
+    }
 
     public void Load(string root)
     {
@@ -26,7 +33,8 @@ public class PluginManager
                 var type = asm.GetTypes().FirstOrDefault(t => typeof(IServicePlugin).IsAssignableFrom(t) && !t.IsAbstract);
                 if (type != null)
                 {
-                    _service = (IServicePlugin)Activator.CreateInstance(type)!;
+                    var plugin = (IServicePlugin)ActivatorUtilities.CreateInstance(_provider, type)!;
+                    _service = (plugin, context, dll);
                     _assemblies.Add(dll);
                     break;
                 }
@@ -44,7 +52,7 @@ public class PluginManager
                 var asm = context.LoadFromAssemblyPath(dll);
                 var type = asm.GetTypes().FirstOrDefault(t => typeof(ICommandHandler).IsAssignableFrom(t) && !t.IsAbstract);
                 if (type == null) continue;
-                var handler = (ICommandHandler)Activator.CreateInstance(type)!;
+                var handler = (ICommandHandler)ActivatorUtilities.CreateInstance(_provider, type)!;
                 _handlers[handler.Name] = (handler, context, dll);
                 _assemblies.Add(dll);
             }
@@ -55,5 +63,5 @@ public class PluginManager
 
     public ICommandHandler? GetHandler(string name) => _handlers.TryGetValue(name, out var value) ? value.Handler : null;
 
-    public IServicePlugin Service => _service ?? throw new InvalidOperationException("Service plugin not loaded");
+    public IServicePlugin Service => _service?.Plugin ?? throw new InvalidOperationException("Service plugin not loaded");
 }
