6f8c76abe58297f4e2718598cde3b7de4ccd2e03
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2025-08-22 00:23:43 +0200

    Add command status result type

--- src/TaskHub.Abstractions/EnqueuedCommandResult.cs
+++ src/TaskHub.Abstractions/EnqueuedCommandResult.cs
@@ -2,4 +2,10 @@ using System;
 
 namespace TaskHub.Abstractions;
 
-public record EnqueuedCommandResult(string Id, string Command, DateTimeOffset EnqueuedAt);
+/// <summary>
+/// Result returned when a command or chain of commands is enqueued.
+/// </summary>
+/// <param name="Id">Identifier of the background job.</param>
+/// <param name="Commands">Commands that will be executed.</param>
+/// <param name="EnqueuedAt">Timestamp when the job was enqueued.</param>
+public record EnqueuedCommandResult(string Id, string[] Commands, DateTimeOffset EnqueuedAt);
